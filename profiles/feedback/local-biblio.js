2ab4091227910a985a5fe00046885b8c0dd1972d
Andrew Gardener <andrew.e.gardener@gmail.com>
2018-07-20 13:17:35 -0700

    Add feedback profile

---
+++ profiles/feedback/local-biblio.js
@@ -0,0 +1,69 @@
+var _localBiblio = {
+  "CALIPER-11": {
+    title: "Caliper® Analytics Specification 1.1",
+    href: "https://www.imsglobal.org/sites/default/files/caliper/v1p1/caliper-spec-v1p1/caliper-spec-v1p1.html",
+    publisher: "IMS Global Learning Consortium",
+    "authors": [
+      "Whyte, Anthony",
+      "Haag, Viktor",
+      "Feng, Linda",
+      "Gylling, Markus",
+      "Ashbourne, Matt",
+      "LaMarche, Wes",
+      "Pelaprat, Etienne"
+    ]
+  },
+  "CALIPER-CERT-11": {
+    title: "Caliper Analytics® Sensor Certification Guide 1.1",
+    href: "https://www.imsglobal.org/sites/default/files/caliper/v1p1/caliper-sensor-cert-guide-v1p1/caliper-sensor-cert-guide-v1p1.html",
+    publisher: "IMS Global Learning Consortium",
+    "authors": [
+      "Whyte, Anthony"
+    ]
+  },
+  "CALIPER-IMPL-11": {
+    title: "Caliper Analytics® Implementation Guide 1.1 @@@ TBD URL",
+    href: "https://www.imsglobal.org/sites/default/files/caliper/v1p1/caliper-implementation-guide-v1p1/caliper-implementation-guide-v1p1.html",
+    publisher: "IMS Global Learning Consortium",
+    "authors": [
+      "@@@, TBD"
+    ]
+  },
+  "CALIPER-FEEDBACK-11": {
+    title: "Caliper Analytics® Feedback Profile 1.1",
+    href: "https://www.imsglobal.org/spec/caliper-feedback/v1p1/",
+    publisher: "IMS Global Learning Consortium",
+    "authors": [
+      "Gardener, Andrew"
+    ]
+  },
+  "CALIPER-FEEDBACK-11-CONTEXT": {
+    title: "Caliper Analytics® Feedback Profile Context 1.1",
+    href: "https://purl.imsglobal.org/spec/caliper-feedback/v1p1/context/",
+    publisher: "IMS Global Learning Consortium"
+  },
+  "CALIPER-FEEDBACK-11-ERRATA": {
+    title: "Caliper Analytics® Feedback Profile 1.1 Errata",
+    href: "https://www.imsglobal.org/spec/caliper-feedback/v1p1/errata/",
+    publisher: "IMS Global Learning Consortium"
+  },
+  "CALIPER-TL-11": {
+    title: "Caliper Analytics® Tool Launch Profile 1.1",
+    href: "https://www.imsglobal.org/spec/caliper-tl/v1p1/",
+    publisher: "IMS Global Learning Consortium",
+    "authors": [
+      "Gylling, Markus",
+      "Whyte, Anthony"
+    ]
+  },
+  "CALIPER-TL-11-CONTEXT": {
+    title: "Caliper Analytics® Tool Launch Profile Context 1.1",
+    href: "https://purl.imsglobal.org/spec/caliper-tl/v1p1/context/",
+    publisher: "IMS Global Learning Consortium"
+  },
+  "CALIPER-TL-11-ERRATA": {
+    title: "Caliper Analytics® Tool Launch Profile 1.1 Errata",
+    href: "https://www.imsglobal.org/spec/caliper-tl/v1p1/errata/",
+    publisher: "IMS Global Learning Consortium"
+  }
+};
