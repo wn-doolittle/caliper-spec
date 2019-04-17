95540e8bd94d0d7c065c588dce168ede89c4745f
Anthony Whyte <anthwhyte@gmail.com>
2019-04-17 15:54:28 -0400

    Add/update/trim local-biblio.js.

--- profiles/resource_management/local-biblio.js
+++ profiles/resource_management/local-biblio.js
@@ -1,71 +1,21 @@
 var _localBiblio = {
-  "CALIPER-11": {
-    title: "Caliper® Analytics Specification 1.1",
-    href: "https://www.imsglobal.org/sites/default/files/caliper/v1p1/caliper-spec-v1p1/caliper-spec-v1p1.html",
-    publisher: "IMS Global Learning Consortium",
-    "authors": [
-      "Whyte, Anthony",
-      "Haag, Viktor",
-      "Feng, Linda",
-      "Gylling, Markus",
-      "Ashbourne, Matt",
-      "LaMarche, Wes",
-      "Pelaprat, Etienne"
-    ]
-  },
-  "CALIPER-CERT-11": {
-    title: "Caliper Analytics® Sensor Certification Guide 1.1",
-    href: "https://www.imsglobal.org/sites/default/files/caliper/v1p1/caliper-sensor-cert-guide-v1p1/caliper-sensor-cert-guide-v1p1.html",
-    publisher: "IMS Global Learning Consortium",
-    "authors": [
-      "Whyte, Anthony"
-    ]
-  },
-  "CALIPER-IMPL-11": {
-    title: "Caliper Analytics® Implementation Guide 1.1 @@@ TBD URL",
-    href: "https://www.imsglobal.org/sites/default/files/caliper/v1p1/caliper-implementation-guide-v1p1/caliper-implementation-guide-v1p1.html",
-    publisher: "IMS Global Learning Consortium",
-    "authors": [
-      "@@@, TBD"
-    ]
-  },
   "CALIPER-RESOURCE-MANAGEMENT-11": {
-    title: "Caliper Analytics® Resource Management Profile 1.1",
-    href: "https://www.imsglobal.org/spec/caliper-resource-management/v1p1/",
-    publisher: "IMS Global Learning Consortium",
+    "title": "Caliper Analytics® Resource Management Profile 1.1",
+    "href": "https://www.imsglobal.org/spec/caliper-resource-management/v1p1/",
+    "publisher": "IMS Global Learning Consortium",
     "authors": [
-      "Whyte, Anthony",
-      "Ashbourne, Matt",
-      "Gardener, Andrew"
+      "Gardener, Andrew",
+      "Whyte, Anthony"
     ]
   },
   "CALIPER-RESOURCE-MANAGEMENT-11-CONTEXT": {
-    title: "Caliper Analytics® Resource Management Profile Context 1.1",
-    href: "https://purl.imsglobal.org/spec/caliper-resource-management/v1p1/context/",
-    publisher: "IMS Global Learning Consortium"
+    "title": "Caliper Analytics® Resource Management Profile Context 1.1 Extension",
+    "href": "https://purl.imsglobal.org/spec/caliper-resource-management/v1p1/context/",
+    "publisher": "IMS Global Learning Consortium"
   },
   "CALIPER-RESOURCE-MANAGEMENT-11-ERRATA": {
-    title: "Caliper Analytics® Resource Management Profile 1.1 Errata",
-    href: "https://www.imsglobal.org/spec/caliper-resource-management/v1p1/errata/",
-    publisher: "IMS Global Learning Consortium"
-  },
-  "CALIPER-TL-11": {
-    title: "Caliper Analytics® Tool Launch Profile 1.1",
-    href: "https://www.imsglobal.org/spec/caliper-tl/v1p1/",
-    publisher: "IMS Global Learning Consortium",
-    "authors": [
-      "Gylling, Markus",
-      "Whyte, Anthony"
-    ]
-  },
-  "CALIPER-TL-11-CONTEXT": {
-    title: "Caliper Analytics® Tool Launch Profile Context 1.1",
-    href: "https://purl.imsglobal.org/spec/caliper-tl/v1p1/context/",
-    publisher: "IMS Global Learning Consortium"
-  },
-  "CALIPER-TL-11-ERRATA": {
-    title: "Caliper Analytics® Tool Launch Profile 1.1 Errata",
-    href: "https://www.imsglobal.org/spec/caliper-tl/v1p1/errata/",
-    publisher: "IMS Global Learning Consortium"
+    "title": "Caliper Analytics® Resource Management Profile 1.1 Errata",
+    "href": "https://www.imsglobal.org/spec/caliper-resource-management/v1p1/errata/",
+    "publisher": "IMS Global Learning Consortium"
   }
 };
