c09f26cbf92ae6219cf8ab30f318b9c58692ea0f
Anthony Whyte <anthwhyte@gmail.com>
2019-04-19 11:52:31 -0400

    Add title, section 1.1 and 1.3.  Add local-biblio.js.

---
+++ local-biblio.js
@@ -0,0 +1,51 @@
+var _localBiblio = {
+    "CALIPER-12": {
+        "title": "Caliper® Analytics Specification 1.2",
+        "href": "https://www.imsglobal.org/sites/default/files/caliper/v1p2/caliper-spec-v1p2/caliper-spec-v1p2.html",
+        "publisher": "IMS Global Learning Consortium",
+        "authors": [
+            "Whyte, Anthony",
+            "Haag, Viktor",
+            "Feng, Linda",
+            "Gylling, Markus",
+            "Ashbourne, Matt",
+            "LaMarche, Wes",
+            "Pelaprat, Etienne"
+        ]
+    },
+
+    "CALIPER-12-CERT": {
+        "title": "Caliper Analytics® Sensor Certification Guide 1.2",
+        "href": "https://www.imsglobal.org/sites/default/files/caliper/v1p2/caliper-sensor-cert-guide-v1p2/caliper-sensor-cert-guide-v1p2.html",
+        "publisher": "IMS Global Learning Consortium",
+        "authors": [
+            "Mosbacker, Bracken",
+            "Whyte, Anthony"
+        ]
+    },
+
+    "CALIPER-12-CONTEXT": {
+        "title": "Caliper Analytics® JSON-LD Context 1.2",
+        "href": "https://purl.imsglobal.org/spec/v1p2/context/",
+        "publisher": "IMS Global Learning Consortium",
+        "authors": [
+            "Whyte, Anthony"
+        ]
+    },
+
+    "CALIPER-12-ERRATA": {
+        "title": "Caliper Analytics® Specification 1.2 Errata",
+        "href": "https://www.imsglobal.org/spec/v1p2/errata/",
+        "publisher": "IMS Global Learning Consortium"
+    },
+
+    "CALIPER-12-IMPL": {
+        "title": "Caliper Analytics® Sensor Implementation Guide 1.2",
+        "href": "https://www.imsglobal.org/sites/default/files/caliper/v1p2/caliper-sensor-impl-guide-v1p2/caliper-sensor-impl-guide-v1p2.html",
+        "publisher": "IMS Global Learning Consortium",
+        "authors": [
+            "Mosbacker, Bracken",
+            "Whyte, Anthony"
+        ]
+    }
+};
